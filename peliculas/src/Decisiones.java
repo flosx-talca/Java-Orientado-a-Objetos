014169c828c256ccde04ccbd0fcd231e1b7fb7d2
flosx-talca <orlandorozasi@gmail.com>
2024-09-16 12:48:32 -0300

    avanzando

--- peliculas/src/Decisiones.java
+++ peliculas/src/Decisiones.java
@@ -4,11 +4,19 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
+
         if (fechaDeLanzamiento >= 2022) {
             System.out.println("Peliculas más populares");
         }
         else {
             System.out.println("Peliculas Retro que aun valen la pena ver12");
+        }
+        if (incluidoEnElPlan ||  tipoPlan.equals("plus")) {
+            System.out.println("Disfrute de su pelicula");
+        }
+        else {
+            System.out.println("Pelicula no disponible");
         }
 
 
