1d18b0b76b669cc7610f1be719f81251e6eb776d
flosx-talca <orlandorozasi@gmail.com>
2024-09-16 14:45:43 -0300

    agregando Loops

---
+++ peliculas/src/Loops.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double nota = 0;
+        double  mediaEvaluaciones=0;
+
+        for (int i = 0; i < 3 ; i++) {
+            System.out.println("Digite una nota" + (i + 1) + ": ");
+            nota = teclado.nextDouble();
+            mediaEvaluaciones = mediaEvaluaciones + nota;
+        }
+        mediaEvaluaciones = mediaEvaluaciones / 3;
+        System.out.println("La media de evaluaciones para matrix es : " + mediaEvaluaciones);
+    }
+}
