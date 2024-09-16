fb8a9b82f8427611919d9235d4d03c55c3a01c90
flosx-talca <orlandorozasi@gmail.com>
2024-09-16 16:08:46 -0300

    Programa cuenta corriente

---
+++ javadesafio/src/Desafio.java
@@ -0,0 +1,12 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre="Tony Stark";
+        String tipoDeCuenta="Cuenta corriente";
+        double saldo = 1599.99;
+        System.out.println("********************************");
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Tipo de Cuenta: "+tipoDeCuenta);
+        System.out.println("Saldo disponible es: "+saldo);
+    }
+
+}
