6f0bc184f3514e58aea44db90883e7ec75162625
flosx-talca <orlandorozasi@gmail.com>
2024-09-24 17:31:35 -0300

    herencia y sobreescritura

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,5 @@
 import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
 
@@ -8,17 +9,22 @@ public class Principal {
         miPelicula.setFechaDeLanzamiento(2021);
         miPelicula.setDuracionEnMinutos(120);
         miPelicula.setIncluidoEnElPlan(true);
-
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         miPelicula.evalua(7.8);
-
         System.out.println("Total de evaluaciones: "+miPelicula.getTotalDelasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-
-        System.out.printf("solo nombre "+miPelicula.getNombre());
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("Dragon");
+        casaDragon.setFechaDeLanzamiento(2021);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+               // System.out.printf("solo nombre "+miPelicula.getNombre());
 
 
     }
