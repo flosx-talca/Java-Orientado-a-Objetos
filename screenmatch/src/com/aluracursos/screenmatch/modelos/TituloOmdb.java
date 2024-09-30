88702c0eb607da64a879c0e205193aa18db7479b
flosx-talca <orlandorozasi@gmail.com>
2024-09-30 12:12:32 -0300

    json a clase
    pasando de json a clases por medio de un Record, generando un constructor y parseando los campos que deben ser enteros

---
+++ screenmatch/src/com/aluracursos/screenmatch/modelos/TituloOmdb.java
@@ -0,0 +1,5 @@
+package com.aluracursos.screenmatch.modelos;
+// RECORD para usar nombres de los json distinto a las clases
+public record TituloOmdb(String title, String year, String runtime) {
+
+}
--- screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ screenmatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,5 +1,11 @@
 package com.aluracursos.screenmatch.principal;
 
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.aluracursos.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,8 +29,15 @@ public class PrincipalConBusqueda{
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create(); // esta funcion es para respetrar las variables en minusculas, peor que el json trae las claves en mayus
+        //Titulo miTitulo =  gson.fromJson(json, Titulo.class);
+        TituloOmdb miTituloOmdb =  gson.fromJson(json, TituloOmdb.class); //usando el recor TiruloOmdb, un recor es parecido a una clase pero mas simple
+        System.out.println(miTituloOmdb);
+        Titulo  miTitulo = new Titulo(miTituloOmdb);
+        System.out.println(miTitulo);
 
-        System.out.println(response.body());
 
     }
 
